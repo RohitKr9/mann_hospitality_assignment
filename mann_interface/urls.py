754ea5b2e9ec4889311e6db59fc8e4cb321aedbe
RohitKr9 <rohitpoddar937@gmail.com>
2025-05-05 17:29:03 +0530

    Improvement

--- mann_interface/urls.py
+++ mann_interface/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload', views.fileUploadIndex),
+    path('', views.fileUploadIndex),
     path('file_upload', views.fileUpload),
     path('mail_summary', views.sendMailWithSummary),
     path('download_filtered', views.downloadFilteredFile),
